117f3a081cdd89bed56d309858c1cbf48ecdf79f
AhmedNafie <ahmedtareknafie@gmail.com>
2022-04-20 14:57:16 +0200

    Added root view for NavigationController

--- AuthenticationApp/SignUpVC.swift
+++ AuthenticationApp/SignUpVC.swift
@@ -7,12 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignUpVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("signup vc")
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
