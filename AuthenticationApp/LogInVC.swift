97abd6e96f71f5f769929e1c3774fc3c67192c3c
AhmedNafie <ahmedtareknafie@gmail.com>
2022-04-20 15:24:21 +0200

    Added The IBAction To go to the ProfileVC.

--- AuthenticationApp/LogInVC.swift
+++ AuthenticationApp/LogInVC.swift
@@ -9,4 +9,10 @@ import UIKit
 
 class LogInVc: UIViewController {
     
+    @IBAction func logInButtonTapped() {
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        let upComingVC = storyBoard.instantiateViewController(withIdentifier: "ProfileVC")
+        navigationController?.pushViewController(upComingVC, animated: true)
+    }
+    
 }
