2cc7c4d53f9c067aeaf5347d86a6027b5f7ef44a
AhmedNafie <ahmedtareknafie@gmail.com>
2022-04-21 18:16:29 +0200

    added ib outlets for all textfields in SignupVC and created the User struct

--- AuthenticationApp/SignUpVC.swift
+++ AuthenticationApp/SignUpVC.swift
@@ -12,12 +12,24 @@ enum Gender: String {
     case female = "Female"
 }
 
+struct User {
+    let name: String
+    let gender: Gender
+    let email: String
+    let password: String
+}
+
 class SignUpVC: UIViewController {
+    
     // MARK: - private Outlets
+    @IBOutlet weak var nameTextFIeld: UITextField!
     @IBOutlet weak private var genderLabel: UILabel!
     @IBOutlet weak var genderSwtich: UISwitch!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
     // MARK: - private Actions
-
     @IBAction private func signUpButtonTapped() {
         let logInVC = storyboard?.instantiateViewController(withIdentifier: "LogInVC")
         navigationController?.pushViewController(logInVC!, animated: true)
@@ -25,7 +37,7 @@ class SignUpVC: UIViewController {
     
     @IBAction private func genderSwtichTapped() {
         genderLabel.text = genderSwtich.isOn ? Gender.male.rawValue : Gender.female.rawValue
-}
+    }
 }
 
 
