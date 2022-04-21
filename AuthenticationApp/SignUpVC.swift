17c1dcf5925b1b8c176ed4dde235eb6fd7310671
AhmedNafie <ahmedtareknafie@gmail.com>
2022-04-21 18:10:34 +0200

    Added the marks and changed the switch action implementation from if to ternary

--- AuthenticationApp/SignUpVC.swift
+++ AuthenticationApp/SignUpVC.swift
@@ -6,28 +6,26 @@
 //
 
 import UIKit
+
 enum Gender: String {
     case male = "Male"
     case female = "Female"
 }
 
 class SignUpVC: UIViewController {
-    
+    // MARK: - private Outlets
     @IBOutlet weak private var genderLabel: UILabel!
     @IBOutlet weak var genderSwtich: UISwitch!
-    
+    // MARK: - private Actions
+
     @IBAction private func signUpButtonTapped() {
         let logInVC = storyboard?.instantiateViewController(withIdentifier: "LogInVC")
         navigationController?.pushViewController(logInVC!, animated: true)
     }
     
     @IBAction private func genderSwtichTapped() {
-        if genderSwtich.isOn {
-            genderLabel.text = Gender.male.rawValue
-        } else {
-            genderLabel.text = Gender.female.rawValue
-        }
-    }
+        genderLabel.text = genderSwtich.isOn ? Gender.male.rawValue : Gender.female.rawValue
+}
 }
 
 
