a43f00eaea427ee224214a85bc4ce68cc933ed3d
AhmedNafie <ahmedtareknafie@gmail.com>
2022-04-24 18:10:14 +0200

    added the missing file

---
+++ AuthenticationApp/Extensions/String+IsValidMail.swift
@@ -0,0 +1,14 @@
+//
+//  String+IsValidMail.swift
+//  AuthenticationApp
+//
+//  Created by Ahmed Nafie on 24/04/2022.
+//
+
+import Foundation
+
+extension String {
+    var isValidEmail: Bool {
+        NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}").evaluate(with: self)
+    }
+}
