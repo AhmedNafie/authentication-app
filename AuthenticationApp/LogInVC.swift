a7e6fadd98232cdff9c0f3ff34c37763d54312ce
AhmedNafie <ahmedtareknafie@gmail.com>
2022-04-28 00:25:54 +0200

    Fixed PR comments

--- AuthenticationApp/LogInVC.swift
+++ AuthenticationApp/LogInVC.swift
@@ -62,9 +62,9 @@ private extension LogInVC {
     func goToProfileVC(with user: User) {
         let profileVC = storyboard?.instantiateViewController(withIdentifier: "ProfileVC") as! ProfileVC
         profileVC.user = user
-        navigationController?.pushViewController(profileVC, animated: true, completion: {
+        navigationController?.pushViewController(profileVC, animated: true) {
             self.navigationController?.viewControllers = [profileVC]
-        })
+        }
     }
 }
 
