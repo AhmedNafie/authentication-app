85435635b881314bec19fa5b814e42c5caaedd3b
AhmedNafie <101005449+AhmedNafie@users.noreply.github.com>
2022-06-19 19:19:17 +0200

    Added TableView For User Info At The ProfileVC (#22)
    * Added tableView for user info at the profileVC
    
    * Fixed PR comments

--- AuthenticationApp/Controller/ProfileVC.swift
+++ AuthenticationApp/Controller/ProfileVC.swift
@@ -9,16 +9,21 @@ import UIKit
 
 class ProfileVC: UIViewController {
     // MARK: - Outlets
-    @IBOutlet weak private var nameLabel: UILabel!
-    @IBOutlet weak private var emailLabel: UILabel!
-    @IBOutlet weak private var genderLabel: UILabel!
     @IBOutlet weak private var imageView: UIImageView!
+    @IBOutlet weak private var userInformationTableView: UITableView!
+
+    // MARK: - Properties
+    private let cellData = [
+        (title: "Name", detail: UserDefaults.standard.string(forKey: "AAName")),
+        (title: "Email", detail: UserDefaults.standard.string(forKey: "AAEmail")),
+        (title: "Gender", detail: UserDefaults.standard.string(forKey: "AAGender"))
+    ]
 
     // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        showUserInfo()
         showImage()
+        setupTableView()
     }
     
     // MARK: - Actions
@@ -28,16 +33,28 @@ class ProfileVC: UIViewController {
     }
 }
 
-//MARK: Private methods
-private extension ProfileVC {
-    func showUserInfo() {
-        nameLabel.text = UserDefaults.standard.string(forKey: "AAName")
-        emailLabel.text = UserDefaults.standard.string(forKey: "AAEmail")
-        genderLabel.text = UserDefaults.standard.string(forKey: "AAGender")
+//MARK: UITableView DataSource 
+ extension ProfileVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        cellData.count
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RightDetailCell", for: indexPath)
+        cell.textLabel?.text = cellData[indexPath.row].title
+        cell.detailTextLabel?.text = cellData[indexPath.row].detail
+        return cell
+    }
+}
+
+//MARK: Private methods
+private extension ProfileVC {
     func showImage() {
         guard let data = UserDefaults.standard.data(forKey: "AAImage") else { return }
         imageView.image = UIImage(data: data)
+    }
+    
+    func setupTableView() {
+        userInformationTableView.dataSource = self
     }
 }
