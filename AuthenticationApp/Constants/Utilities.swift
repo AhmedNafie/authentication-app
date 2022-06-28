0c036a2d8dbc9ed15ddacb4e105d740944673490
AhmedNafie <ahmedtareknafie@gmail.com>
2022-06-28 20:34:14 +0200

    Added userDefaults keys

---
+++ AuthenticationApp/Constants/Utilities.swift
@@ -0,0 +1,19 @@
+//
+//  Utilities.swift
+//  AuthenticationApp
+//
+//  Created by Ahmed Nafie on 28/06/2022.
+//
+
+import Foundation
+
+class DataPersistenceManager {
+    struct UserDefaultsKeys {
+        let name = "AAName"
+        let email = "AAEmail"
+        let gender = "AAGender"
+        let password = "AAPassword"
+        let image = "AAImage"
+        let login = "AAlogin"
+    }
+}
